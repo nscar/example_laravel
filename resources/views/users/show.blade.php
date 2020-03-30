93c9d85a75fd339bf6a83df14da13de2ec2ab9d4
nscar <nsotocar@gmail.com>
2020-03-30 13:54:15 -0300

    example laravel 7_h

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,20 +3,30 @@
 @section('content')
 <div class="container">
     @if ($user->id === auth()->id())
-        <div class="alert alert-info">
-            You are seeing your own profile in public mode.
-            <a href="{{ route('home') }}">Click here</a> to manage your entries and tweets.
-        </div>
+    <div class="alert alert-info">
+        You are seeing your own profile in public mode.
+        <a href="{{ route('home') }}">Click here</a> to manage your entries and tweets.
+    </div>
     @endif
 
     <div class="row justify-content-center">
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header">Tweets</div>
-
+                @if ($twitterConnected)
+                <ul class="list-group list-group-flush">
+                    @foreach ($tweets as $tweet)
+                    <li class="list-group-item">
+                        <p><small>{{ $tweet['created_at'] }}</small></p>
+                        <p>{{ $tweet['text'] }}</p>
+                    </li>
+                    @endforeach
+                </ul>
+                @else
                 <div class="card-body">
-                    Twitter API
+                    This user has not linked his or her Twitter account yet.
                 </div>
+                @endif
             </div>
         </div>
 
@@ -28,9 +38,9 @@
                     <p>Published entries:</p>
                     <ul>
                         @foreach ($entries as $entry)
-                            <li>
-                                <a href="{{ $entry->getUrl() }}">{{ $entry->title }}</a>
-                            </li>
+                        <li>
+                            <a href="{{ $entry->getUrl() }}">{{ $entry->title }}</a>
+                        </li>
                         @endforeach
                     </ul>
                 </div>
@@ -39,5 +49,3 @@
     </div>
 </div>
 @endsection
-
-
